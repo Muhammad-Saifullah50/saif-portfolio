44ca23d245d4137a76a3b9ddc735a90620e928af
Muhammad-Saifullah50 <saifullahm2005@gmail.com>
2023-10-13 10:43:02 +0500

    hided feedback section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,7 +12,7 @@ export default function Home() {
       <Tech />
       <Education/>
       <Projects />
-      <Feedbacks />
+      {/* <Feedbacks /> */}
       <section className='relative z-0'>
         <Contact />
         <StarsCanvas />
