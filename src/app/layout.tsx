1c3c738bf3f901d61d62ef130bc4a42dd16fba1b
Muhammad-Saifullah50 <saifullahm2005@gmail.com>
2023-11-14 11:56:39 +0500

    removed inter

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,6 @@
 import './globals.css'
 import type { Metadata } from 'next'
-import { Poppins } from 'next/font/google'
 
-const inter = Poppins({ subsets: ['latin'], weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'] })
 
 export const metadata: Metadata = {
   title: 'Developer Saif',
@@ -16,7 +14,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className={inter.className}>
+      <body>
         {children}
       </body>
 
